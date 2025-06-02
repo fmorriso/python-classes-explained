8fe39c7a3465c51b798a49e4e67f2d45a0819211
Fred Morrison <frederick.morrison@outlook.com>
2025-06-01 20:33:39 -0600

    update software levels in use

--- main.py
+++ main.py
@@ -7,10 +7,7 @@ from guitar import Guitar
 def get_python_version() -> str:
     return f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}'
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print(f'Python versioun {get_python_version()}')
+def main():
     my_guitar = Guitar()
     print(my_guitar.numStrings)
     print(my_guitar.getNumStrings())
@@ -20,3 +17,8 @@ if __name__ == '__main__':
     my_guitar.playLouder()
     print(f'Child class: {my_guitar.numStrings}')
     print(f'Parent class {Guitar().numStrings}')
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(f'Python version {get_python_version()}')
+
--- readme.md
+++ readme.md
@@ -3,16 +3,17 @@
 
 | Tool     |  Version |
 |:---------|---------:|
-| Python   | 3.13.0   |
-| PyCharm  | 2024.2.3 |
-| VSCode   | 1.94.2   |
+| Python   |   3.13.3 |
+| PyCharm  | 2025.1.1 |
+| VSCode   |  1.100.2 |
 
 ## Change History
 
-| Date       | Description      |
-|:-----------|:-----------------|
-| 2024-10-18 | Initial creation |
+| Date       | Description          |
+|:-----------|:---------------------|
+| 2024-10-18 | Initial creation     |
+| 2025-06-01 | add requirements.txt |
 
 ##References
+
 * [video: OOP Class Inheritance and Private Class Members - Python for Beginners!](https://www.youtube.com/watch?v=6c6NYPjO_rI)
-* 
